5aa3471062f74965196919ad2103638bad03937c
Sebastião Emidio Alves Filho <sebastiao.alves@gmail.com>
2021-07-26 10:52:08 -0300

    Exemplos do URI da semana 01

---
+++ semana01/uri-1003.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b; // Também poderia ser: cin >> a >> b;
+    cout << "SOMA = " << a+b << endl;
+    return 0;
+}
